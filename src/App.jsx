f214667f2d0fe8da9e37d1d8b5bf75066d13937f
oseicute08-sudo <oseicute08@gmail.com>
2026-09-23 15:45:56 +0000

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from './context/ThemeContext';
+import { ProgressProvider } from './context/ProgressContext';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import Courses from './pages/Courses';
+import CourseDetail from './pages/CourseDetail';
+import Lesson from './pages/Lesson';
+import QuizCenter from './pages/QuizCenter';
+import QuizTake from './pages/QuizTake';
+import Dictionary from './pages/Dictionary';
+import ProgressPage from './pages/Progress';
+import Profile from './pages/Profile';
+
+export default function App() {
+  return (
+    <ThemeProvider>
+      <ProgressProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="courses" element={<Courses />} />
+              <Route path="courses/:courseId" element={<CourseDetail />} />
+              <Route path="courses/:courseId/lessons/:lessonId" element={<Lesson />} />
+              <Route path="quiz" element={<QuizCenter />} />
+              <Route path="quiz/:quizId" element={<QuizTake />} />
+              <Route path="dictionary" element={<Dictionary />} />
+              <Route path="progress" element={<ProgressPage />} />
+              <Route path="profile" element={<Profile />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </ProgressProvider>
+    </ThemeProvider>
+  );
+}
