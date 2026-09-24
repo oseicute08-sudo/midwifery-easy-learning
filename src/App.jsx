da41bfd5a7022f28b7ef76bb3526a1eab14bbbf6
oseicute08-sudo <oseicute08@gmail.com>
2026-09-24 23:11:01 +0000

    Refactor routing to include AuthProvider and RequireOwner

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
 import { ProgressProvider } from './context/ProgressContext';
+import { AuthProvider } from './context/AuthContext';
 import Layout from './components/Layout';
+import RequireOwner from './components/RequireOwner';
 import Home from './pages/Home';
 import Courses from './pages/Courses';
 import CourseDetail from './pages/CourseDetail';
@@ -11,27 +13,42 @@ import QuizTake from './pages/QuizTake';
 import Dictionary from './pages/Dictionary';
 import ProgressPage from './pages/Progress';
 import Profile from './pages/Profile';
+import Login from './pages/Login';
+import OwnerDashboard from './pages/OwnerDashboard';
+import StudyGuide from './pages/StudyGuide';
 
 export default function App() {
   return (
     <ThemeProvider>
       <ProgressProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<Home />} />
-              <Route path="courses" element={<Courses />} />
-              <Route path="courses/:courseId" element={<CourseDetail />} />
-              <Route path="courses/:courseId/lessons/:lessonId" element={<Lesson />} />
-              <Route path="quiz" element={<QuizCenter />} />
-              <Route path="quiz/:quizId" element={<QuizTake />} />
-              <Route path="dictionary" element={<Dictionary />} />
-              <Route path="progress" element={<ProgressPage />} />
-              <Route path="profile" element={<Profile />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
+        <AuthProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route path="courses" element={<Courses />} />
+                <Route path="courses/:courseId" element={<CourseDetail />} />
+                <Route path="courses/:courseId/lessons/:lessonId" element={<Lesson />} />
+              <Route path="courses/:courseId/lessons/:lessonId/study" element={<StudyGuide />} />
+                <Route path="quiz" element={<QuizCenter />} />
+                <Route path="quiz/:quizId" element={<QuizTake />} />
+                <Route path="dictionary" element={<Dictionary />} />
+                <Route path="progress" element={<ProgressPage />} />
+                <Route path="profile" element={<Profile />} />
+                <Route path="login" element={<Login />} />
+                <Route
+                  path="owner"
+                  element={
+                    <RequireOwner>
+                      <OwnerDashboard />
+                    </RequireOwner>
+                  }
+                />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </AuthProvider>
       </ProgressProvider>
     </ThemeProvider>
   );
-}
+                  }
